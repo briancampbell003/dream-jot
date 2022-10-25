3f2a0cd244fead3747eceaf375d9bbcd8d0d18d7
Mingazhutdin <mingadag204@gmail.com>
2022-10-24 21:33:35 -0500

    "created post request"

--- controllers/api/dream-routes.js
+++ controllers/api/dream-routes.js
@@ -1,20 +1,19 @@
-const router = require('express').Router();
-const { Dream } = require('../../models');
+const router = require("express").Router();
+const { Dream } = require("../../models");
 
 // CREATE new dream
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const dbDreamData = await Dream.create({
-        title: req.body.title,
-        content: req.body.content,
-        tag: req.body.tag,
-        lucid: req.body.lucid,
-        /* check it later*/ 
-        user_id: req.body.user_id,
-        
-      });
-      // console.log(dbDreamData)
-      req.session.save(() => {
+      title: req.body.title,
+      content: req.body.content,
+      tag: req.body.tag,
+      lucid: req.body.lucid,
+      /* check it later*/
+      user_id: req.body.user_id,
+    });
+    // console.log(dbDreamData)
+    req.session.save(() => {
       req.session.loggedIn = true;
 
       res.status(200).json(dbDreamData);
@@ -24,5 +23,8 @@ router.post('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+
+
 
 module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
--- Active: 1664495632750@@127.0.0.1@3306@project2_db
+-- Active: 1664495643342@@127.0.0.1@3306@minga
 DROP DATABASE IF EXISTS project2_db;
 
 CREATE DATABASE project2_db;
